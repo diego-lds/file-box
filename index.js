8c6894db9ceb7a08696b5df96b369630b5d5b178
Diego Lopes <diegolopes087@gmail.com>
2024-06-02 10:35:27 -0300

    upload + read

--- index.js
+++ index.js
@@ -9,29 +9,58 @@ import {
   DeleteBucketCommand,
   paginateListObjectsV2,
   GetObjectCommand,
+  ListObjectsCommand,
 } from "@aws-sdk/client-s3";
 
+const credentials = {
+  accessKeyId: "YIBXBX5B22Y4PZISWERY",
+  secretAccessKey: "oi00WUXuCoyXtvLwoAK8B1WtmWbBQqsRfe5iNoUy",
+};
+const client = new S3Client({
+  endpoint: "https://br-gru-1.linodeobjects.com",
+  region: "br-gru-1",
+  credentials: credentials,
+});
+
+const params = {
+  Bucket: "file-box",
+  Key: "file.txt",
+  Body: "sou arquivo",
+  ACL: "public-read",
+};
 export async function main() {
-  const credentials = {
-    accessKeyId: "YIBXBX5B22Y4PZISWERY",
-    secretAccessKey: "oi00WUXuCoyXtvLwoAK8B1WtmWbBQqsRfe5iNoUy",
-  };
+  // uploadObject(client, params);
+  // deleteObject(client, params);
+  listObjects(client, params);
+}
 
-  const s3Client = new S3Client({
-    endpoint: "https://br-gru-1.linodeobjects.com",
-    region: "br-gru-1",
-    credentials: credentials,
-  });
-
-  const params = {
-    Bucket: "file-box",
-    Key: "file.txt",
-    Body: "sou arquivo",
-    ACL: "public-read",
-  };
-
+async function uploadObject(client, params) {
   try {
-    await s3Client.send(new PutObjectCommand(params));
+    await client.send(new PutObjectCommand(params));
+    console.log(
+      `arquivo ${params.Key} criado com sucesso em [${params.bucket}]`
+    );
+  } catch (error) {
+    console.log(`erro ao fazer upload de arquivo`, error);
+  }
+}
+async function deleteObject(client, params) {
+  try {
+    await client.send(new DeleteObjectCommand(params));
+    console.log(
+      `arquivo ${params.Key} criado com sucesso em [${params.bucket}]`
+    );
+  } catch (error) {
+    console.log(`erro ao fazer upload de arquivo`, error);
+  }
+}
+
+async function listObjects(client, params) {
+  try {
+    const { Contents } = await client.send(new ListObjectsCommand(params));
+    for (let obj of Contents) {
+      console.log(obj.Key);
+    }
   } catch (error) {}
 }
 
